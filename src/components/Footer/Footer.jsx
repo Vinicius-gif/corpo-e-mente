36d61b985c9bf533ca4de163606bfa74e9b7abf1
Vinicius <vfpv@hotmail.com>
2023-11-03 20:47:44 -0300

    add footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-green-500 py-4 fixed bottom-0 left-0 right-0 px-4">
+      <div className="container mx-auto flex items-center justify-between">
+        <p className="text-white text-sm">&copy; {new Date().getFullYear()} Corpo & Mente</p>
+        <nav>
+          <ul className="flex space-x-4">
+            <li><a href="#" className="text-white">Política de Privacidade</a></li>
+            <li><a href="#" className="text-white">Termos de Uso</a></li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
