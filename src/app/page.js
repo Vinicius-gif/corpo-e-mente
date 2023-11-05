5fb334556c5d7a435b5b905f5c012e430c2c5f3b
Vinicius <vfpv@hotmail.com>
2023-11-05 18:34:57 -0300

    add banner

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Banner from '@/components/Banner/Banner';
 import Benefits from '@/components/Benefits/Benefits';
 import FeaturedContent from '@/components/FeaturedContent/FeaturedContent';
 
 export default function Home() {
   return (
     <main>
+      <Banner/>
       <Benefits/>
       <FeaturedContent/>
     </main>
