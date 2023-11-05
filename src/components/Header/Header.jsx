0f1e7a0a03421018456f349ae543d9d6efb2ca67
Vinicius <vfpv@hotmail.com>
2023-11-05 18:36:44 -0300

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
 import React from 'react';
-import Banner from './Banner/Banner';
 
 const Header = () => {
   return (
     <>
       <header className="bg-green-500 py-4">
         <div className="container mx-auto flex items-center justify-between">
-          <a href="#" className="text-white text-2xl font-bold">Corpo & Mente</a>
+          <Link href="/" className="text-white text-2xl font-bold">Corpo & Mente</Link>
           <nav>
             <ul className="flex space-x-4">
               <li><a href="#" className="text-white">Pensamento Saudável</a></li>
@@ -17,7 +17,6 @@ const Header = () => {
           </nav>
         </div>
       </header>
-      <Banner/>
     </>
   );
 };
