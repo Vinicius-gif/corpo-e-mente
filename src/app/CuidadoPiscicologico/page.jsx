04fa4c74bc416fa83fef60a24a9312bd9b3efc55
Vinicius <vfpv@hotmail.com>
2023-11-06 20:51:31 -0300

    adicionando conteúdo e styles

--- src/app/CuidadoPiscicologico/page.jsx
+++ src/app/CuidadoPiscicologico/page.jsx
@@ -1,8 +1,59 @@
+import Image from 'next/image';
 import React from 'react';
 
 const PensamentoSaudavel = () => {
   return (
-    <div>PensamentoSaudavel</div>
+    <div className="p-10 grid grid-cols-1 justify-center justify-items-center gap-y-12">
+      <Image src="/images/dicas-especialistas.png" alt="dicas" width={700} height={700}/>
+      <h2 className="mt-11 font-medium text-3xl border-b-2 border-green-500 pb-8">Dicas importantes para um autocuidado psicológico completo</h2>
+      <div>
+        <section>
+          <h3 className="text-lg font-medium">Autocuidado psicológico:</h3>
+          <ol className="list-decimal pl-4 my-5">
+            <li>Escreva um diário</li>
+            <li>Observe sua experiencia interior- perceber seus pensamentos, julgamentos, crenças, atitudes e sentimentos</li>
+            <li>Diga “não” as responsabilidades extras, às vezes é respeitar os próprios limites</li>
+            <li>Busque ajuda quando não conseguir resolver questões que promovem o sofrimento interno</li>
+          </ol>
+        </section>
+        <section>
+          <h3 className="text-lg font-medium">Autocuidado físico:</h3>
+          <ol className="list-decimal pl-4 my-5">
+            <li>Coma regularmente e de forma saudável</li>
+            <li>Exercitar-se, correr, andar</li>
+            <li>Dormir o suficiente</li>
+            <li>Dar um tempo de celular e computadores</li>
+          </ol>
+        </section>
+        <section>
+          <h3 className="text-lg font-medium">Autocuidado Emocional:</h3>
+          <ol className="list-decimal pl-4 my-5">
+            <li>Mantenha contato com pessoas importantes na sua vida</li>
+            <li>Seja afirmativo(a) consigo, elogiar-se</li>
+            <li>Encontre coisas que te façam rir</li>
+            <li>Aproxime de pessoas, e lugares que te fazem bem</li>
+          </ol>
+        </section>
+        <section>
+          <h3 className="text-lg font-medium">Autocuidado espiritual:</h3>
+          <ol className="list-decimal pl-4 my-5">
+            <li>Encontre um tempo para refletir</li>
+            <li>Meditar e orar</li>
+            <li>Esteja aberto à inspiração</li>
+            <li>Passar algum tempo com amigos(as)</li>
+          </ol>
+        </section>
+        <section>
+          <h3 className="text-lg font-medium">Autocuidado em casa, faculdade ou em local de trabalho:</h3>
+          <ol className="list-decimal pl-4 my-5">
+            <li>Faça pausas durante a jornada da faculdade/ escola ou trabalho</li>
+            <li>Reserve um tempo de silêncio para concluir as tarefas</li>
+            <li>Equilibre suas atividades para que nenhum dia te sobrecarregue</li>
+            <li>Busque equilíbrio dentro da sua vida da faculdade/escola ou na rotina de trabalho, em amigos, relacionamentos e família</li>
+          </ol>
+        </section>
+      </div>
+    </div>
   );
 };
 
--- src/app/Relaxamento/page.jsx
+++ src/app/Relaxamento/page.jsx
@@ -1,8 +1,69 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Relaxamento = () => {
   return (
-    <div>Relaxamento</div>
+    <div className="p-10 px-28 grid grid-cols-1 justify-center justify-items-center gap-y-12">
+      <h2 className="mt-11 font-medium text-3xl border-b-2 border-green-500 pb-8">
+        Como prática relaxamento corporal e mental, para uma qualidade de vida melhor
+      </h2>
+      <section>
+        <p>Se você tem uma rotina agitada no trabalho, sente estresse e ansiedade, é importante fazer
+        você fazer uma pausa e diminuir um pouco o seu ritmo. As técnicas de relaxamento elas
+        podem te ajudar a manter o equilíbrio, evitar o esgotamento e outros problemas mais
+        graves, geralmente ligado ao excesso de trabalho. Uma pessoa descansada e com a mente
+        tranquila produz muito mais do que alguém que está com a cabeça cheia de preocupações
+        e sem foco.
+        </p>
+        <p>
+        Separamos melhores técnicas de relaxamento do corpo e da mente que prometem ajudar
+        na redução do estresse diário. A seguir, veja quais são elas
+        </p>
+      </section>
+      <section>
+        <h3 className="my-7 text-2xl font-semibold">Automassagem</h3>
+        <section className="text-base">
+          <p>
+          Uma das melhores maneiras de combater o estresse, é fazendo alguns intervalos durante
+          o trabalho, praticando a automassagem, mesmo que você só utilize 5 ou 15 minutos do
+          seu tempo. A massagem é uma técnica eficaz que melhora a sua qualidade de vida, sendo
+          usada para a redução das dores e desconfortos. Leva a uma sensação de relaxamento
+          melhorando a sua produtividade na sua ocupação e em atividades no dia a dia.
+          </p>
+          <p>Segue abaixo passo a passo de como fazer a automassagem no ambiente de trabalho.</p>
+        </section>
+        <div className="flex justify-center">
+          <Image className="my-10" alt="" src="" width={500} height={500}/>
+        </div>
+      </section>
+      <section>
+        <h3 className="my-7 text-2xl font-semibold">Meditação</h3>
+        <ul className="list-disc">
+          <li>
+            <h4 className="my-7 text-xl font-medium">Meditação Concentrativa:</h4>
+            <p>
+              Se caracteriza pela atenção direcionada a um único foco, como a respiração, um
+              mantra ou algum som. Nesse caso, podem ser observados aspectos sensoriais,
+              como a nossa respiração (o ambiente é ignorado e a atenção é direcionada para o
+              ato de respirar), ou aspectos mentais, como a nossa própria consciência.
+            </p>
+          </li>
+          <li>
+            <h4 className="my-7 text-xl font-medium">Meditação Mindfulness ou atenção plena:</h4>
+            <p>Caracterizada pela consciência da experiência do momento presente, com uma
+              atitude de aceitação, em que nenhum tipo de elaboração ou julgamento é utilizado.
+              Exercitando a atenção plena podemos focar no que estamos vivendo no momento
+              de forma que nossa produtividade em relação a qualquer tarefa, seja produtiva e
+              satisfatória.
+            </p>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h3 className="my-7 text-2xl font-semibold text-center">Dicas para começar a meditar</h3>
+        <Image className="my-10" alt="" src="" width={500} height={500}/>
+      </section>
+    </div>
   );
 };
 
