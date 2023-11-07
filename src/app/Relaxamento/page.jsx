b4dee3fdcee09de9c35cb6489c7f32c44bfe9fc9
Vinicius-gif <vfpv@hotmail.com>
2023-11-07 09:55:21 -0300

    update page

--- src/app/Relaxamento/page.jsx
+++ src/app/Relaxamento/page.jsx
@@ -61,7 +61,75 @@ const Relaxamento = () => {
       </section>
       <section>
         <h3 className="my-7 text-2xl font-semibold text-center">Dicas para começar a meditar</h3>
-        <Image className="my-10" alt="" src="" width={500} height={500}/>
+        <div className="flex justify-center">
+          <Image className="my-10" alt="" src="" width={500} height={500}/>
+        </div>
+        <ol className="list-decimal [&>li]:py-6">
+          <li>
+            <p>
+            Você pode meditar pela manhã, antes de ir para o trabalho, no horário de almoço,
+            para recarregar as energias para o resto do dia, à noite, ao chegar em casa, ou um
+            pouco antes de ir dormir. Para quem está começando, o ideal é iniciar com pouco
+            tempo: cinco a dez minutos já é o suficiente.
+            </p>
+          </li>
+          <li>
+            <p>
+            Defina um local para praticar, pode ser no seu quarto, na sala ou em outro
+            cantinho da sua casa ou trabalho. O importante é que seja um lugar calmo e
+            silencioso.
+            </p>
+          </li>
+          <li>
+            <p>
+            É possível meditar sentado, deitado ou em pé. Cabe a você escolher o modo mais
+            confortável e começar sua meditação. Encontre uma posição confortável,
+            mantendo a coluna reta, os ombros relaxados e o pescoço alinhado
+            </p>
+          </li>
+          <li>
+            <p>
+            Inspire profundamente, puxando o ar e inflando a barriga e o tórax. Em seguida,
+            expire lentamente, com calma. Para facilitar esse exercício, experimente contar
+            até cinco na inspiração e na expiração
+            </p>
+          </li>
+          <li>
+            <p>
+            Uma ótima dica é colocar uma playlist de sons da natureza para direcionar seu
+            foco. Concentre-se na música e deixe os pensamentos fluírem, aceite-os e deixe-
+            os ir embora.
+            </p>
+          </li>
+        </ol>
+        <p className="py-9 font-medium">
+          Há muitas formas de meditar e, com tantos benefícios, vale a pena tentar encaixar a prática
+          no dia a dia. Com o tempo, a meditação poderá trazer mais foco e determinação para a
+          sua rotina de treinos.
+        </p>
+        <div className="flex justify-center">
+          <Image className="my-10" alt="" src="" width={500} height={500}/>
+        </div>
+        <h3 className="my-7 text-2xl font-semibold">
+          Exercício Físico
+        </h3>
+        <p>
+          O estilo de vida ativo e a prática regular de atividades físicas pode auxiliar no tratamento
+          e prevenção de problemas psicológicos, como depressão e ansiedade além de ser bom
+          para aliviar o estresse. A prática de exercícios físicos aeróbicos como caminhada, andar
+          de bicicleta, natação, corrida, dentre outros, pode ajudar e muito na melhora de nossa saúde mental.
+        </p>
+        <div className="flex justify-center">
+          <Image className="my-10" alt="" src="" width={500} height={500}/>
+        </div>
+        <p className="pb-12">
+          Outros meios de estar relaxando corpo e a mente é você se dedicar a um trabalho
+          voluntário que lhe dê plena satisfação, cuidar de uma horta, jogar com os amigos e
+          familiares, ouvir uma música que te deixe relaxar, ler um livro, cantar e manejar um
+          instrumento musical. Você também pode está procurando um profissional para tá
+          aplicando uma massagem relaxante e acupuntura que te traz ótimos benefícios para a
+          saúde física e mental.
+        </p>
       </section>
     </div>
   );
