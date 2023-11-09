95606f394fff8745b1d35050fb0ca68966c8881f
Vinicius-gif <vfpv@hotmail.com>
2023-11-09 08:18:30 -0300

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,13 +8,6 @@ const Header = () => {
       <header className="bg-green-500 py-4">
         <div className="container mx-auto flex items-center justify-between">
           <Link href="/" className="text-white text-2xl font-bold">Corpo & Mente</Link>
-          <nav>
-            <ul className="flex space-x-4">
-              <li><a href="#" className="text-white">Pensamento Saudável</a></li>
-              <li><a href="#" className="text-white">Dicas de Saúde</a></li>
-              <li><a href="#" className="text-white">Bem-Estar Mental</a></li>
-            </ul>
-          </nav>
         </div>
       </header>
     </>
